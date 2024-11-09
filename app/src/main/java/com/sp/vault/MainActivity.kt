5f45222c4dcfcb01b1983b78898e1be4861aaab6
Mo <mohammedkhambhati2020@gmail.com>
2024-11-09 16:37:12 +0800

    first version of UI

--- app/src/main/java/com/sp/vault/MainActivity.kt
+++ app/src/main/java/com/sp/vault/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.sp.vault.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeScreen())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeScreen())
+                R.id.profile -> replaceFragment(ProfileScreen())
+                else -> {
+                }
+
+            }
+            true
         }
+
     }
+
+    fun replaceFragment(fragement : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FragmentContainerView, fragement)
+        fragmentTransaction.commit()
+    }
+
 }
